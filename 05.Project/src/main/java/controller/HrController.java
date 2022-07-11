c7e383e9c689d761fcfdaeb2b7eec9a97ed32eb0
YshinH <tlsgid666@gmail.com>
2022-07-11 18:09:49 +0900

    no message

--- 05.Project/src/main/java/controller/HrController.java
+++ 05.Project/src/main/java/controller/HrController.java
@@ -1,4 +1,4 @@
-package controller;
+package Controller;
 
 import java.io.IOException;
 
@@ -9,38 +9,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import hr.HrDAO;
+import dept.DeptDAO;
+import emp.EmpDAO;
 
 
-@WebServlet({"*.emp", "*.dept"})
+@WebServlet(urlPatterns = {"*.emp","*.dept"})
 public class HrController extends HttpServlet {
-
+	EmpDAO empDAO = new EmpDAO();
+	DeptDAO deptDAO = new DeptDAO();
+	
 	RequestDispatcher rd;
+	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		HrDAO dao = new HrDAO();
-	
+		System.out.println(req.getServletPath());
+		
 		if(req.getServletPath().equals("/list.emp")) {
-			System.out.println("emp");
-			
-			 dao.getEmpList();
-			 System.out.println(dao.getEmpList().size());
-			 
-			 req.setAttribute("list", dao.getEmpList());
+			//DAO 만들고 req.setAtrr해야함
+			System.out.println(empDAO.getList().size());
+			System.out.println("사원 리스트 출력");
+			req.setAttribute("emplist", empDAO.getList());
 			rd = req.getRequestDispatcher("hr/emplist.jsp");
-	
-		}else if(req.getServletPath().equals("/list.dept")){
-			System.out.println("dept");
 			
-			dao.getDepList();
-			System.out.println(dao.getDepList().size());
-			 
-			 req.setAttribute("list", dao.getDepList());
-			
-			rd = req.getRequestDispatcher("hr/deptlist.jsp");
+		}else if(req.getServletPath().equals("/list.dept")) {
+			System.out.println(deptDAO.getList().size());
+			System.out.println("부서 리스트 출력");
+			req.setAttribute("deptlist", deptDAO.getList());
+			rd = req.getRequestDispatcher("hr/deptlist.jsp");			
 		}
-		
 		rd.forward(req, resp);
-		
-	}
-}
+	}//service
+}//class
